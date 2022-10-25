4b84897dbbdee90849d651d0f5db9d1a05f69924
YZahorodnii <yzagorodniy@gmail.com>
2022-10-25 18:16:17 +0100

    Finished copy range() function

--- ft_lib/ft_range.py
+++ ft_lib/ft_range.py
@@ -1,11 +1,12 @@
-
 def ft_range(stop, start=0, step=1):
-    l = []
-    while start < stop:
-        if start <= stop:
-            l.append(start)
-        else:
-            l = []
-        start += step
-    print(l)
-
+    if isinstance(stop, int) and isinstance(start, int) and isinstance(step, int):
+        l = []
+        while start < stop:
+            if start <= stop:
+                l.append(start)
+            else:
+                return l
+            start += step
+        return l
+    else:
+        print('Value must be int')
