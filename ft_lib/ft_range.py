2d96967cc7d30db29732f58da2c0face72ac6bcb
YZahorodnii <yzagorodniy@gmail.com>
2022-10-25 18:33:58 +0100

    Changed variable name l to res for

--- ft_lib/ft_range.py
+++ ft_lib/ft_range.py
@@ -1,12 +1,12 @@
 def ft_range(stop, start=0, step=1):
     if isinstance(stop, int) and isinstance(start, int) and isinstance(step, int):
-        l = []
+        res = []
         while start < stop:
             if start <= stop:
-                l.append(start)
+                res.append(start)
             else:
-                return l
+                return res
             start += step
-        return l
+        return res
     else:
         print('Value must be int')
