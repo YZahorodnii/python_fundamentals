76c09419297d919e828038ab7ace552598053a19
YZahorodnii <yzagorodniy@gmail.com>
2022-10-30 13:55:06 +0000

    Refacturing code done

--- ft_lib/ft_upper.py
+++ ft_lib/ft_upper.py
@@ -1,14 +1,11 @@
-
 def ft_upper(obj):
-    l = ''
+    res = ''
     if isinstance(obj, str):
         for i in obj:
             if ord(i) in range(97, 123):
-                j = ord(i) - 32
-                l = l + chr(j)
+                res = res + chr(ord(i) - 32)
             else:
-                j = ord(i)
-                l = l + chr(j)
-        return l
+                res = res + chr(ord(i))
+        return res
     else:
         print(f'You passed {type(obj)} instead of str required')
