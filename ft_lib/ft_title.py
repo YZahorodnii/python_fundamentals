6fa0064c44707bc1428b1accc74fc514439aaa98
YZahorodnii <yzagorodniy@gmail.com>
2022-10-30 14:00:10 +0000

    Code refacturing done

--- ft_lib/ft_title.py
+++ ft_lib/ft_title.py
@@ -12,7 +12,6 @@ def ft_title(obj):
                         res = res + chr(ord(item) - 32)
                     else:
                         res = res + chr(ord(item))
-
                     start = True
                 elif (ord(item) in range(97, 123) or ord(item) in range(65, 91)) and start:
                     if ord(item) in range(97, 123):
